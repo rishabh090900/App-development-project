cccd506b655c016e5ce91b1335cefb86fc7483b2
ShreyaSingh-24 <72667409+ShreyaSingh-24@users.noreply.github.com>
2020-11-20 13:34:52 +0530

    Add files via upload

---
+++ src/main/java/com/example/signup/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.signup;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        // If savedinstnacestate is null then replace login fragment
+        if (savedInstanceState == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frameContainer, new Login_Fragment(),
+                            Utils.Login_Fragment).commit();
+        }
+
+        // On close icon click finish activity
+        findViewById(R.id.close_activity).setOnClickListener(
+                new OnClickListener() {
+
+                    @Override
+                    public void onClick(View arg0) {
+                        finish();
+
+                    }
+                });
+
+    }
+
+    // Replace Login Fragment with animation
+    protected void replaceLoginFragment() {
+        fragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.left_enter, R.anim.right_out)
+                .replace(R.id.frameContainer, new Login_Fragment(),
+                        Utils.Login_Fragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // Find the tag of signup and forgot password fragment
+        Fragment signup_fragment = fragmentManager
+                .findFragmentByTag(Utils.signup_fragment);
+        Fragment ForgotPassword_Fragment = fragmentManager
+                .findFragmentByTag(Utils.ForgotPassword_Fragment);
+
+        // Check if both are null or not
+        // If both are not null then replace login fragment else do backpressed
+        // task
+
+        if (signup_fragment != null)
+            replaceLoginFragment();
+        else if (ForgotPassword_Fragment != null)
+            replaceLoginFragment();
+        else
+            super.onBackPressed();
+    }
+}
---
+++ src/main/java/com/example/signup/Utils.java
@@ -0,0 +1,13 @@
+package com.example.signup;
+
+public class Utils {
+    //Email Validation pattern
+    public static final String regEx = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+$";
+
+    //Fragments Tags
+    public static final String Login_Fragment = "Login_Fragment";
+    public static final String signup_fragment = "signup_fragment";
+    public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+
+}
+
